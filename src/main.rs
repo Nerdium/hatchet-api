23b6b623802a8140a51cb33137fc4fab3120bd2a
nerdium <nerdiumlol@gmail.com>
2024-05-13 14:47:46 -0500

    update address to be available outside of container

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000.");
+    let listener = TcpListener::bind("0.0.0.0:8000").expect("Failed to bind port 8000.");
     run(listener)?.await
 }
